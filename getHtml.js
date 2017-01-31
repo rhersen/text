42d3067394567fb989c5f41c7fd2a5f841dd6fed
Rudolf Hersén <rudolf.hersen@schibsted.se>
2017-01-31 16:37:09 +0100

    Simplified implementation since we only show one direction at a time

--- getHtml.js
+++ getHtml.js
@@ -8,27 +8,19 @@ import formatLatestAnnouncement from './formatLatestAnnouncement'
 import * as position from './position'
 
 export default function getHtml(announcements, stationNames, lastModified) {
-    let s = '<div id="sheet">'
+    let s = `<div id="sheet"><h1>${lastModified}</h1>`
 
-    const latest = map(groupby(announcements, 'AdvertisedTrainIdent'), v => maxby(v, 'TimeAtLocation'))
+    const trains = map(groupby(announcements, 'AdvertisedTrainIdent'), v => maxby(v, 'TimeAtLocation'))
 
-    foreach(groupby(latest, direction), (trains, dir) => {
-        s += `<h1>${dir} ${lastModified}</h1>`
+    trains.sort((a, b) => position.y(a.LocationSignature) - position.y(b.LocationSignature))
 
-        trains.sort((a, b) => position.y(a.LocationSignature) - position.y(b.LocationSignature))
-
-        foreach(trains, a => {
-            s += `<div style="color: ${delay.color(a)}; text-align: ${position.x(a.LocationSignature)};">`
-            s += `${formatLatestAnnouncement(a, stationNames)}`
-            s += '</div>'
-        })
-
+    foreach(trains, a => {
+        s += `<div style="color: ${delay.color(a)}; text-align: ${position.x(a.LocationSignature)};">`
+        s += `${formatLatestAnnouncement(a, stationNames)}`
+        s += '</div>'
     })
 
     s += '</div>'
     return s
 }
 
-function direction(t) {
-    return /[13579]$/.test(t.AdvertisedTrainIdent) ? 'söderut' : 'norrut'
-}
