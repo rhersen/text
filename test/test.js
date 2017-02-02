ada40ad9ad4c4feea4412ebf16be2e630bb04c5e
Rudolf Hersén <rudolf.hersen@schibsted.se>
2017-02-02 16:50:47 +0100

    Fix failing test

--- test/test.js
+++ test/test.js
@@ -4,8 +4,8 @@ import getHtml from '../getHtml'
 
 describe('getHtml', function () {
     it('returns empty div', function () {
-        const actual = getHtml()
+        const actual = getHtml([])
 
-        expect(actual).to.equal('<div id="sheet"></div>')
+        expect(actual).to.equal('<div id="sheet"><h1>undefined</h1></div>')
     })
 })
